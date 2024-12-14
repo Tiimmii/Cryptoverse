63288edbad20b5e8d573efa9a12d6b0329380ffd
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-12-14 13:14:04 +0100

    set up footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,57 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route, Link, useLocation } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd'; 
-import Navbar from './components/Navbar';
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
 function App() {
   const location = useLocation();
   // npm install antd @ant-design/icons react-redux @reduxjs/toolkit axios chart.js html-react-parser millify moment react-chartjs-2
 
   return (
     <>
-      <div className='app'>
+      <div className="app">
         <div className="navbar">
-          <Navbar/>
+          <Navbar />
         </div>
         <div className="main">
           <Layout>
             <div className="routes">
               <Routes location={location} key={location.pathname}>
-                <Route path='/' element={''}/>
-                <Route path='/exchanges' element={''}/>
-                <Route path='/cryptocurrencies' element={''}/>
-                <Route path='/crypto/:coinId' element={''}/>
-                <Route path='/news' element={''}/>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/exchanges" element={<Exchanges />} />
+                <Route
+                  path="/cryptocurrencies"
+                  element={<Cryptocurrencies />}
+                />
+                <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                <Route path="/news" element={<News />} />
               </Routes>
             </div>
           </Layout>
-        </div>
-        <div className="footer">
-
+          <div className="footer">
+            <Typography.Title
+              level={5}
+              style={{ color: "white", textAlign: "center" }}
+            >
+              Cryptoverse <br />
+              All rights reserved
+            </Typography.Title>
+            <Space>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/exchanges"}>Exchanges</Link>
+              <Link to={"/cryptocurrencies"}>Cryptocurrencies</Link>
+              <Link to={"/News"}>News</Link>
+            </Space>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
